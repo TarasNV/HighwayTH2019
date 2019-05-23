e610ff8ac947d385ffe1d3e64afd8d51cae008db
Taras <taras.nadtochii@gmail.com>
2019-05-23 23:33:51 +0300

    [Trello] update page objects

--- src/main/java/trello/LoggedOutPage.java
+++ src/main/java/trello/LoggedOutPage.java
@@ -1,4 +1,22 @@
 package trello;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static core.BrowserFactory.driver;
+
 public class LoggedOutPage {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    private By loginButton = By.xpath("//a[@class='global-header-section-button']");
+
+    public boolean isOpened() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='global-header-section-button']")));
+        if (driver.findElement(loginButton).isDisplayed())
+            return true;
+        else
+            return false;
+    }
+
+
 }
